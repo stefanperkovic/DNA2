eb2a8fe953fce0b726ff7774aa0ecf8b44cf7bd3
stefanperkovic <stefan.perkovic@menloschool.org>
2024-10-14 09:54:51 -0700

    Continued thinking and started code.

--- src/DNA.java
+++ src/DNA.java
@@ -6,7 +6,7 @@
  * at Menlo School in Atherton, CA
  *</p>
  * <p>
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Stefan Perkovic
  *</p>
  */
 
@@ -16,6 +16,45 @@ public class DNA {
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
     public static int STRCount(String sequence, String STR) {
+        int[] map = new int[STR.length()];
+        map[0] = 00;
+        map[2] = 01;
+        map[19] = 10;
+        map[6] = 11;
+        int seq = 0;
+
+
+        int maxcount = 0;
+        for (int i = 0; i < STR.length(); i++){
+            if (STR.charAt(i) == 'A'){
+
+
+            }
+            else if (STR.charAt(i) == 'C') {
+
+            }
+            else if (STR.charAt(i) == 'T') {
+
+            }
+            else{
+
+            }
+
+        }
+
+
+
+
+        for (int i = 0; i < sequence.length(); i++){
+
+
+
+
+        }
+
+
+
+
 
         return 0;
     }

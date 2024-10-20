657773df99de7ff37a05a8d74f0d0d52f2ed9350
stefanperkovic <stefan.perkovic@menloschool.org>
2024-10-19 17:46:54 -0700

    Finished structure, but still getting errors.

--- src/DNA.java
+++ src/DNA.java
@@ -19,43 +19,48 @@ public class DNA {
 
     public static int STRCount(String sequence, String STR) {
 
+        // Calculates STR value
         long STR_hash = 0;
         for (int i = 0; i < STR.length(); i++){
             STR_hash = (STR_hash * BASE + (STR.charAt(i) - 'A')) % MOD;
         }
 
+        // Calculates value of first str.length() characters
         long current_hash = 0;
         for (int i = 0; i < STR.length(); i++){
             current_hash = (current_hash * BASE + (sequence.charAt(i) - 'A')) % MOD;
         }
 
+        // Calculates value of base^str.length() to simply later calculations
+        long base_power = 1;
+        for (int i = 0; i < STR.length(); i++){
+            base_power = (base_power * BASE) % MOD;
+        }
+        // Makes it equal to str.length() - 1
+        base_power /= BASE;
 
         int current_count = 0;
         int max_count = 0;
-        for (int i = 0; i < sequence.length() - STR.length(); i++){
+        for (int i = STR.length(); i <= sequence.length() - 1; i++){
 
             if (current_hash == STR_hash){
                 current_count++;
-
             }
             else{
+                if (max_count < current_count){
+                    max_count = current_count;
+                }
                 current_count = 0;
             }
-
-
-
-            if (max_count < current_count){
-                max_count = current_count;
+            if (i < STR.length()){
+                current_hash = ((current_hash + MOD) - (STR.charAt(i - STR.length()) * base_power) % MOD) % MOD;
+                current_hash = ((current_hash * BASE) + (sequence.charAt(i) - 'A')) % MOD;
             }
-
-
 
         }
 
 
 
-
-
-        return 0;
+        return max_count;
     }
 }

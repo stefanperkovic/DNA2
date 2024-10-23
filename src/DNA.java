98b253ebdc97ca0d42ceee9e4bb7f95fd7be77eb
stefanperkovic <stefan.perkovic@menloschool.org>
2024-10-23 09:58:53 -0700

    Trying to debug the error in my code.

--- src/DNA.java
+++ src/DNA.java
@@ -19,43 +19,45 @@ public class DNA {
 
     public static int STRCount(String sequence, String STR) {
 
+        if (sequence.length() < STR.length()){
+            return 0;
+        }
+
+
         // Calculates STR value
         long STR_hash = 0;
-        for (int i = 0; i < STR.length(); i++){
-            STR_hash = (STR_hash * BASE + (STR.charAt(i) - 'A')) % MOD;
+        for (int i = 0; i < STR.length(); i++) {
+            STR_hash = (STR_hash * BASE + STR.charAt(i)) % MOD;
         }
 
         // Calculates value of first str.length() characters
         long current_hash = 0;
-        for (int i = 0; i < STR.length(); i++){
-            current_hash = (current_hash * BASE + (sequence.charAt(i) - 'A')) % MOD;
+        for (int i = 0; i < STR.length(); i++) {
+            current_hash = (current_hash * BASE + sequence.charAt(i)) % MOD;
         }
 
         // Calculates value of base^str.length() to simply later calculations
         long base_power = 1;
-        for (int i = 0; i < STR.length(); i++){
+        for (int i = 1; i < STR.length(); i++){
             base_power = (base_power * BASE) % MOD;
         }
-        // Makes it equal to str.length() - 1
-        base_power /= BASE;
 
         int current_count = 0;
         int max_count = 0;
-        for (int i = STR.length(); i <= sequence.length() - 1; i++){
+        for (int i = STR.length(); i < sequence.length(); i++){
 
             if (current_hash == STR_hash){
                 current_count++;
-            }
-            else{
                 if (max_count < current_count){
                     max_count = current_count;
                 }
+            }
+            else{
                 current_count = 0;
             }
-            if (i < STR.length()){
-                current_hash = ((current_hash + MOD) - (STR.charAt(i - STR.length()) * base_power) % MOD) % MOD;
-                current_hash = ((current_hash * BASE) + (sequence.charAt(i) - 'A')) % MOD;
-            }
+            current_hash = ((current_hash + MOD) - (sequence.charAt(i - STR.length()) * base_power) % MOD) % MOD;
+            current_hash = ((current_hash * BASE) + sequence.charAt(i)) % MOD;
+
 
         }
 

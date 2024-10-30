ec781bbb4394b882b0780f49243e0f208bd53d9f
stefanperkovic <stefan.perkovic@menloschool.org>
2024-10-29 23:31:10 -0700

    Created calculateHash for abstraction.

--- src/DNA.java
+++ src/DNA.java
@@ -17,6 +17,16 @@ public class DNA {
     private static final int BASE = 256;
     private static final long MOD = 1000000007;
 
+
+    // Calculates the hash for a substring starting at index 0 up to length
+    private static long calculateHash(String s, int len) {
+        long hash = 0;
+        for (int i = 0; i < len; i++) {
+            hash = (hash * BASE + s.charAt(i)) % MOD;
+        }
+        return hash;
+    }
+
     public static int STRCount(String sequence, String STR) {
 
         // If STR is longer than the sequence, there can be no matches
@@ -25,16 +35,10 @@ public class DNA {
         }
 
         // Calculates the hash value for the STR
-        long STR_hash = 0;
-        for (int i = 0; i < STR.length(); i++) {
-            STR_hash = (STR_hash * BASE + STR.charAt(i)) % MOD;
-        }
+        long STR_hash = calculateHash(STR, STR.length());
 
         // Calculates initial hash of first window in our sequence
-        long current_hash = 0;
-        for (int i = 0; i < STR.length(); i++) {
-            current_hash = (current_hash * BASE + sequence.charAt(i)) % MOD;
-        }
+        long current_hash = calculateHash(sequence, STR.length());
 
         // Compute for efficient rolling hash updates
         long base_power = 1;
@@ -55,10 +59,7 @@ public class DNA {
 
                 // Recalculate hash for the new window if within bounds
                 if (i <= sequence.length() - STR.length()) {
-                    current_hash = 0;
-                    for (int j = 0; j < STR.length(); j++) {
-                        current_hash = (current_hash * BASE + sequence.charAt(i + j)) % MOD;
-                    }
+                    current_hash = calculateHash(sequence.substring(i, i + STR.length()), STR.length());
                 }
             }
             // Track the maximum count of consecutive matches
